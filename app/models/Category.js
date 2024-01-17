43bd3fa01e32129d2b84eca9198204054ab6e341
sourisooo <119761249+sourisooo@users.noreply.github.com>
2024-01-17 11:28:28 +0100

    Create Category.js

---
+++ app/models/Category.js
@@ -0,0 +1,25 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('../database');
+
+class Category extends Sequelize.Model {}
+/***
+ * Voici les champs nécessaires pour le Model
+ * name string
+ * tableName: 'categories',
+ */
+
+Category.init(
+    {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+    },
+    {
+        sequelize,
+        tableName: 'categories',
+    }
+);
+
+module.exports = Category;
