698502be7156a52b65702d03dcc235062fe8b4f1
sourisooo <119761249+sourisooo@users.noreply.github.com>
2024-01-17 14:37:10 +0100

    Update cartController.js

--- app/controllers/cartController.js
+++ app/controllers/cartController.js
@@ -5,69 +5,7 @@ const cartController = {
         res.render('cart');
     },
 
-    addOrUpdate: async (req, res) => {
-
-        console.log('testadd');
-
-        console.log(req.session);
-
-        const productId = parseInt(req.params.productId);
-
-
-
-        const productsInCart = req.session.cart;
-        const productToAdd = await Product.findOne({
-            where: { id: productId },
-        });
-
-        console.log(req.session);
-
-        // Si on a déjà le produit dans le panier , on met à jour la quantité, sinon on ajoute le produit au panier
-        const found = productsInCart.find(
-            prod => parseInt(prod.id) === productToAdd.id
-        );
-
-        if (found) {
-            found['qty'] += 1;
-            req.session.cart.products = productsInCart.map(prod =>
-                prod.id === found.id ? found : prod
-            );
-        } else {
-            productToAdd.dataValues['qty'] = 1;
-            req.session.cart.products.push(productToAdd);
-        }
-
-        res.redirect('/shop');
-    },
-
-    remove: (req, res) => {
-
-        const {id} = req.params;
-
-        // const productId = parseInt(req.params.productId);
-
-    
-        const productsInCart = req.session.cart;
-        const newProducts = productsInCart.filter(
-            prod => prod.id != id
-        );
-
-        console.log(newProducts);
-
-        console.log(req.session.cart);
-
-        req.session.cart = newProducts;
-
-        res.redirect('/cart');
-    },
-
-    destroy: (req, res) => {
-        req.session.cart = {};
-        req.session.cart['products'] = [];
-        res.locals.cart = req.session.cart;
-
-        res.redirect('/shop');
-    },
+  
 };
 
 module.exports = cartController;
