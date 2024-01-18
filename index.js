b8127bcaa2ce9d1907aeafb7725d0766ad4aebd4
sourisooo <119761249+sourisooo@users.noreply.github.com>
2024-01-18 20:28:14 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app = express();
 const session = require('express-session');
 const path = require('path');
 
+const pgSession = require('connect-pg-simple')(session);
+
+
 // Local imports
 const router = require('./app/routers');
 const errorHandlers = require('./middlewares/errorHandlers');
@@ -13,14 +16,40 @@ const loadUserToLocals = require('./middlewares/loadUserToLocals');
 // Body parser
 app.use(express.urlencoded({ extended: true }));
 
+const { Client } = require("pg");
+
+const {DataTypes} = require('sequelize');
+const sequelize = require('./app/database');
+// const queryInterface = sequelize.getQueryInterface();
+
+const pgClient = new Client('postgres://postgres:random@localhost/shoppingcart');
+
+pgClient.connect();
+
+
 // Charger les données de la sessions sur `req.session` et `res.locals`
 app.use(
     session({
         saveUninitialized: true,
         resave: true,
         secret: 'Un secret pour signer les id de sessions',
+     
+
+    // store: new pgSession({ // BONUS : persister la session en BDD Postgres. Ignorer pour une première relecture
+    //     pool : pgClient, // On prend notre connecteur sequelize et on cherche le client PG sous la capot // Soit on re-créer un !
+    //     tableName : 'sessions',
+    //     createTableIfMissing: true,
+
+    //   }),
+    
     })
 );
+
+// sequelize.query('ALTER TABLE "sessions" ADD COLUMN "user_id" INTEGER REFERENCES users("id");', { raw: true });
+
+// sequelize.query('ALTER TABLE "sessions" RENAME COLUMN "sid" TO "id";', { raw: true })
+
+
 app.use(loadUserToLocals);
 
 // Setup view engine
